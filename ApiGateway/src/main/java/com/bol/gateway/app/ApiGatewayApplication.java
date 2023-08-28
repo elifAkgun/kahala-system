0a26656205b71de55b23c242cf2eaeb00862706e
elif.akgun <potrak@gmail.com>
2023-08-28 13:22:51 +0300

    added api gateway and eureka discovery server

---
+++ ApiGateway/src/main/java/com/bol/gateway/app/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.bol.gateway.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
